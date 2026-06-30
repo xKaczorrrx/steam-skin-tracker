64e9c62347716045c51527694150b56fd4566199
xKaczorrrx <piotrekwoje2@gmail.com>
2026-06-30 19:31:49 +0200

    Refactor price fetching and update send message format

--- main.py
+++ main.py
@@ -1,19 +1,38 @@
 import json
 import requests
+import urllib.parse
 
 with open("config.json", "r", encoding="utf-8") as f:
     config = json.load(f)
 
-def get_price(url_name: str):
-    # na razie test
-    return 1.0
+def send(msg):
+    requests.post(config["discord_webhook"], json={"content": msg})
 
-def send(msg: str):
-    webhook = config["discord_webhook"]
-    requests.post(webhook, json={"content": msg})
+def get_price(item_name):
+    url_name = urllib.parse.quote(item_name)
+
+    url = f"https://steamcommunity.com/market/priceoverview/?appid=730&currency=6&market_hash_name={url_name}"
+
+    r = requests.get(url)
+    data = r.json()
+
+    if not data.get("success"):
+        return None
+
+    price = data.get("lowest_price")
+    if not price:
+        return None
+
+    price = price.replace("zł", "").replace("$", "").replace(",", ".").strip()
+
+    try:
+        return float(price)
+    except:
+        return None
+
 
 for item in config["items"]:
     name = item["url_name"]
     price = get_price(name)
 
-    send(f"TEST: {name} = {price}")
+    send(f"STEAM: {name} = {price}")
