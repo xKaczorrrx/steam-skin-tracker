223fee825bbd7a9852095944ebe135fc886d606f
xKaczorrrx <piotrekwoje2@gmail.com>
2026-06-30 18:32:02 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import urllib.parse
 with open("config.json", "r") as f:
     config = json.load(f)
 
-WEBHOOK = config["https://discord.com/api/webhooks/1521552130920681564/SZd5mTBSMWum4q5rWD3OdXzOPVaTu2lCLtQpx_vXeFpa3qsVt1Q57kWc6jtzwRfxwzU7"]
+WEBHOOK = config[discord_webhook]
 
 def get_price(name):
     url = f"https://steamcommunity.com/market/priceoverview/?currency=6&appid=730&market_hash_name={urllib.parse.quote(name)}"
